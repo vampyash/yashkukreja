b3d040de85cdb9ca1cb30452522d397d25ffe229
vampdroid <89890815+vampyash@users.noreply.github.com>
2023-09-03 18:28:59 +0530

    Add homepage styles and completed hero section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Yash Kukreja
-        </p>
+    <div className="main-container">
+      <div className="content-container content-container--hero">
+       <h1 className="title--hero"> Hey! It's Yash Kukreja</h1>
+       <h2 className="sub-title sub-title--hero"> I am a <span className="sub-title sub-title--profession">Content Creator</span> </h2>
+      </div>
+      <header className="main-header">
+        
       </header>
     </div>
   );
